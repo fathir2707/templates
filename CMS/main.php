0235d5387d64d38789a3738bfa89279ab1b22ecb
Fathi Rabbani <fathirabbani2707@gmail.com>
2021-12-04 00:18:55 +0700

    apk customer management system mengolah pelaporan dari pelanggan

---
+++ CMS/main.php
@@ -0,0 +1,7 @@
+<p align="center" style="color:#990000;font-size:16px;font-weight:bold;" >Sistem Manajemen Keluhan Pelanggan</p>
+<img src="images/make-online-complaint-btn.jpg"  style="float:left;border:0px; padding:10px;"/>
+<p align="left" style="line-height:24px; padding:10px;">Sistem Manajemen Keluhan Pelanggan ini adalah proyek berbasis web yang dikembangkan di PHP / MySQL yang digunakan untuk mengelola keluhan pelanggan secara online. Pengguna dapat login, dan membuat keluhan. Sistem ini memiliki satu modul Admin di mana Admin dapat login dan melihat semua rincian keluhan, dan memberi penugasan kepada tim kepuasan pelanggan untuk menetapkan status keluhan tersebut.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
